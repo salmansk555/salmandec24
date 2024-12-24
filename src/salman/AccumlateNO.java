09dd109dc148fdabf1fa2158277353e0591d82f4
salmansk555 <salmanskk158@gmail.com>
2024-12-24 12:34:02 +0530

    first commit

---
+++ src/salman/AccumlateNO.java
@@ -0,0 +1,22 @@
+package salman;
+
+public class AccumlateNO {
+
+	public static void main(String[] args) {
+		int num = 9999,r,sum=0;
+		while(num !=0){
+			r=num%10;
+			num = num/10;
+			sum= sum=r;
+			if(num == 0) {
+				if(sum >=10) {
+					num=sum;
+					sum = 0;
+					
+				}
+			}
+		}
+			System.out.println("sum------->"+sum);
+			
+	}
+		}
